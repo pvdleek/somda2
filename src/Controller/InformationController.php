b2c6db61e223b9c980fcae441708313807111c8d
Peter van der Leek <petervdleek@gmail.com>
2020-05-18 17:44:35 +0200

    Add auto-complete to passingRoutes location input

--- src/Controller/InformationController.php
+++ src/Controller/InformationController.php
@@ -3,19 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Jargon;
-use App\Entity\Location;
 use App\Helpers\TemplateHelper;
 use Doctrine\Persistence\ManagerRegistry;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\Response;
 
 class InformationController
 {
-    const SEARCH_METHOD_CHARACTER = 'letter';
-    const SEARCH_METHOD_SINGLE = 'specifiek';
-    const SEARCH_METHOD_NAME = 'naam';
-    const SEARCH_METHOD_DESCRIPTION = 'omschrijving';
-
     /**
      * @var ManagerRegistry
      */
@@ -34,39 +27,6 @@ class InformationController
     {
         $this->doctrine = $doctrine;
         $this->templateHelper = $templateHelper;
-    }
-
-    /**
-     * @IsGranted("ROLE_ABBREVIATIONS")
-     * @param string|null $searchMethod
-     * @param string|null $search
-     * @return Response
-     */
-    public function locationsAction(string $searchMethod = null, string $search = null): Response
-    {
-        switch($searchMethod) {
-            case self::SEARCH_METHOD_CHARACTER:
-                $locations = $this->doctrine->getRepository(Location::class)->findByName($search . '%');
-                break;
-            case self::SEARCH_METHOD_SINGLE:
-                $locations = $this->doctrine->getRepository(Location::class)->findByName($search);
-                break;
-            case self::SEARCH_METHOD_NAME:
-                $locations = $this->doctrine->getRepository(Location::class)->findByName('%' . $search . '%');
-                break;
-            case self::SEARCH_METHOD_DESCRIPTION:
-                $locations = $this->doctrine->getRepository(Location::class)->findByDescription('%' . $search . '%');
-                break;
-            default:
-                $locations = $this->doctrine->getRepository(Location::class)->findAll();
-                break;
-        }
-
-        return $this->templateHelper->render('information/locations.html.twig', [
-            TemplateHelper::PARAMETER_PAGE_TITLE => 'Verkortingen',
-            'search' => $search,
-            'locations' => $locations,
-        ]);
     }
 
     /**
---
+++ src/Controller/LocationController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Location;
+use App\Helpers\TemplateHelper;
+use App\Helpers\UserHelper;
+use Doctrine\Persistence\ManagerRegistry;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+class LocationController
+{
+    const SEARCH_METHOD_CHARACTER = 'letter';
+    const SEARCH_METHOD_SINGLE = 'specifiek';
+    const SEARCH_METHOD_NAME = 'naam';
+    const SEARCH_METHOD_DESCRIPTION = 'omschrijving';
+
+    /**
+     * @var ManagerRegistry
+     */
+    private ManagerRegistry $doctrine;
+
+    /**
+     * @var UserHelper
+     */
+    private UserHelper $userHelper;
+
+    /**
+     * @var TemplateHelper
+     */
+    private TemplateHelper $templateHelper;
+
+    /**
+     * @param ManagerRegistry $doctrine
+     * @param UserHelper $userHelper
+     * @param TemplateHelper $templateHelper
+     */
+    public function __construct(ManagerRegistry $doctrine, UserHelper $userHelper, TemplateHelper $templateHelper)
+    {
+        $this->doctrine = $doctrine;
+        $this->userHelper = $userHelper;
+        $this->templateHelper = $templateHelper;
+    }
+
+    /**
+     * @IsGranted("ROLE_ABBREVIATIONS")
+     * @param string|null $searchMethod
+     * @param string|null $search
+     * @return Response
+     */
+    public function indexAction(string $searchMethod = null, string $search = null): Response
+    {
+        switch($searchMethod) {
+            case self::SEARCH_METHOD_CHARACTER:
+                $locations = $this->doctrine->getRepository(Location::class)->findByName($search . '%');
+                break;
+            case self::SEARCH_METHOD_SINGLE:
+                $locations = $this->doctrine->getRepository(Location::class)->findByName($search);
+                break;
+            case self::SEARCH_METHOD_NAME:
+                $locations = $this->doctrine->getRepository(Location::class)->findByName('%' . $search . '%');
+                break;
+            case self::SEARCH_METHOD_DESCRIPTION:
+                $locations = $this->doctrine->getRepository(Location::class)->findByDescription('%' . $search . '%');
+                break;
+            default:
+                $locations = $this->doctrine->getRepository(Location::class)->findAll();
+                break;
+        }
+
+        return $this->templateHelper->render('information/locations.html.twig', [
+            TemplateHelper::PARAMETER_PAGE_TITLE => 'Verkortingen',
+            'search' => $search,
+            'locations' => $locations,
+        ]);
+    }
+
+    /**
+     * @param string $search
+     * @return JsonResponse
+     */
+    public function jsonAction(string $search): JsonResponse
+    {
+        /**
+         * @var Location[] $locations
+         */
+        $locations = $this->doctrine->getRepository(Location::class)->findByName($search);
+        if (count($locations) < 1) {
+            $locations = $this->doctrine->getRepository(Location::class)->findByName('%' . $search . '%');
+            if (count($locations) < 1) {
+                $locations = $this->doctrine->getRepository(Location::class)->findByDescription('%' . $search . '%');
+            }
+        }
+        $locations = array_slice($locations, 0, 20);
+
+        $json = [];
+        foreach ($locations as $location) {
+            $json[] = [
+                'id' => $location->getId(),
+                'label' => $location->name . ' - ' . $location->description,
+                'description' => $location->description,
+                'value' => $location->name
+            ];
+        }
+
+        return new JsonResponse($json);
+    }
+}
