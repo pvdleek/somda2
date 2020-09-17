4113eb4aad442ba833ad2209e4db60378ce46300
Peter van der Leek <petervdleek@gmail.com>
2020-09-17 17:38:52 +0200

    Add unit-tests for DateHelper

--- tests/Helpers/DateHelperTest.php
+++ tests/Helpers/DateHelperTest.php
@@ -5,6 +5,7 @@ namespace App\Tests\Helpers;
 use App\Helpers\DateHelper;
 use App\Tests\BaseTestCase;
 use DateTime;
+use Exception;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 class DateHelperTest extends BaseTestCase
@@ -14,40 +15,44 @@ class DateHelperTest extends BaseTestCase
      */
     private DateHelper $object;
 
-    public function testGetDisplayDate(): void
+    /**
+     * @return array
+     */
+    public function getDisplayDateProvider(): array
     {
-        $translatorMock = $this->prophet->prophesize(TranslatorInterface::class);
-        $translatorMock->trans('general.date.monthsShort.1')->willReturn('jan')->shouldBeCalled();
-
-        $this->object = new DateHelper($translatorMock->reveal());
-        $this->assertSame(
-            '1 jan',
-            $this->object->getDisplayDate(DateTime::createFromFormat('Y-m-d', '2020-01-01'), false, true)
-        );
+        return [
+            ['Tuesday 1 january 2020', DateTime::createFromFormat('Y-m-d H:i:s', '2020-01-01 15:16:17'), false, false],
+            [
+                'Tuesday 1 january 2020 15:16:17',
+                DateTime::createFromFormat('Y-m-d H:i:s', '2020-01-01 15:16:17'),
+                true,
+                false
+            ],
+            ['1 jan', DateTime::createFromFormat('Y-m-d H:i:s', '2020-01-01 15:16:17'), false, true],
+            ['1 jan 15:16:17', DateTime::createFromFormat('Y-m-d H:i:s', '2020-01-01 15:16:17'), true, true],
+        ];
     }
 
-    public function getDisplayDate($date, bool $includeTime = false, bool $shortDate = false): string
-    {
-        if (!$date instanceof DateTime) {
-            $date = new DateTime($date);
-        }
+    /**
+     * @param string $expectedOutput
+     * @param DateTime $dateTime
+     * @parma bool $includeTime
+     * @param bool $shortDate
+     * @dataProvider getDisplayDateProvider
+     * @throws Exception
+     */
+    public function testGetDisplayDate(
+        string $expectedOutput,
+        DateTime $dateTime,
+        bool $includeTime,
+        bool $shortDate
+    ): void {
+        $translatorMock = $this->prophet->prophesize(TranslatorInterface::class);
+        $translatorMock->trans('general.date.days.2')->willReturn('Tuesday');
+        $translatorMock->trans('general.date.monthsShort.1')->willReturn('jan');
+        $translatorMock->trans('general.date.months.1')->willReturn('january');
 
-        $output = '';
-        if (!$shortDate) {
-            $output .= $this->translator->trans('general.date.days.' . ($date->format('N') - 1)) . ' ';
-        }
-        $output .= $date->format('j') . ' ';
-
-        $output .= $this->translator->trans(
-            'general.date.months' . ($shortDate ? 'Short' : '') . '.' . $date->format('n')
-        );
-
-        if (!$shortDate) {
-            $output .= ' ' . $date->format('Y');
-        }
-        if ($includeTime) {
-            $output .= ' ' . $date->format('H:i:s');
-        }
-        return $output;
+        $this->object = new DateHelper($translatorMock->reveal());
+        $this->assertSame($expectedOutput, $this->object->getDisplayDate($dateTime, $includeTime, $shortDate));
     }
 }
