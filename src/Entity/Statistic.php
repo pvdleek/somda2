43cbe4b8b85d9bd2942ebc15110f3150bce3ae63
Peter van der Leek <petervdleek@gmail.com>
2023-11-25 10:04:16 +0100

    Add defaults for statistics

--- src/Entity/Statistic.php
+++ src/Entity/Statistic.php
@@ -24,32 +24,32 @@ class Statistic
     public ?\DateTime $timestamp = null;
 
     /**
-     * @ORM\Column(name="uniek", type="integer", nullable=false, options={"unsigned"=true})
+     * @ORM\Column(name="uniek", type="integer", nullable=false, options={"default"=0, "unsigned"=true})
      */
     public int $visitorsUnique = 0;
 
     /**
-     * @ORM\Column(name="pageviews", type="integer", nullable=false, options={"unsigned"=true})
+     * @ORM\Column(name="pageviews", type="integer", nullable=false, options={"default"=0, "unsigned"=true})
      */
     public int $visitorsTotal = 0;
 
     /**
-     * @ORM\Column(name="pageviews_home", type="integer", nullable=false, options={"unsigned"=true})
+     * @ORM\Column(name="pageviews_home", type="integer", nullable=false, options={"default"=0, "unsigned"=true})
      */
     public int $visitorsHome = 0;
 
     /**
-     * @ORM\Column(name="pageviews_func", type="integer", nullable=false, options={"unsigned"=true})
+     * @ORM\Column(name="pageviews_func", type="integer", nullable=false, options={"default"=0, "unsigned"=true})
      */
     public int $visitorsFunctions = 0;
 
     /**
-     * @ORM\Column(name="spots", type="integer", nullable=false, options={"unsigned"=true})
+     * @ORM\Column(name="spots", type="integer", nullable=false, options={"default"=0, "unsigned"=true})
      */
     public int $numberOfSpots = 0;
 
     /**
-     * @ORM\Column(name="posts", type="integer", nullable=false, options={"unsigned"=true})
+     * @ORM\Column(name="posts", type="integer", nullable=false, options={"default"=0, "unsigned"=true})
      */
     public int $numberOfPosts = 0;
 }
