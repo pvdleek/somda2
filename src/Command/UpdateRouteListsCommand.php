9c2b286bba1988bb22d301dc1a4021eec7f4819d
Peter van der Leek <petervdleek@gmail.com>
2024-09-03 21:52:45 +0200

    Fix script

--- src/Command/UpdateRouteListsCommand.php
+++ src/Command/UpdateRouteListsCommand.php
@@ -32,17 +32,17 @@ class UpdateRouteListsCommand extends Command
         $statement->executeStatement();
 
         $query = 'INSERT INTO `somda_tdr_trein_treinnummerlijst` (`treinnummerlijst_id`, `treinid`)
-        SELECT `l`.`id`, `tr`.`treinid`
-            FROM `somda_tdr_treinnummerlijst` `l`
-            JOIN `somda_trein` `tr` ON `tr`.`treinnr` BETWEEN `l`.`nr_start` AND `l`.`nr_eind`
-            JOIN `somda_tdr_s_e` `t` ON `t`.`treinid` = `tr`.`treinid` AND `t`.`tdr_nr` = `l`.`tdr_nr`
-            GROUP BY `tr`.`treinid`, `l`.`id`';
+            SELECT `l`.`id`, `tr`.`treinid`
+                FROM `somda_tdr_treinnummerlijst` `l`
+                JOIN `somda_trein` `tr` ON IFNULL(CAST(REGEXP_REPLACE(`tr`.`treinnr`, \'[^0-9]+\', \'\') AS DOUBLE), NULL) BETWEEN `l`.`nr_start` AND `l`.`nr_eind`
+                JOIN `somda_tdr_s_e` `t` ON `t`.`treinid` = `tr`.`treinid` AND `t`.`tdr_nr` = `l`.`tdr_nr`
+                GROUP BY `tr`.`treinid`, `l`.`id`';
         $statement = $connection->prepare($query);
         $statement->executeStatement();
 
         // Remove all routes that no longer exist
         $query = 'DELETE FROM `somda_tdr_trein_treinnummerlijst`
-			    WHERE `treinnummerlijst_id` NOT IN (SELECT `id` FROM `somda_tdr_treinnummerlijst`)';
+            WHERE `treinnummerlijst_id` NOT IN (SELECT `id` FROM `somda_tdr_treinnummerlijst`)';
         $statement = $connection->prepare($query);
         $statement->executeStatement();
 
